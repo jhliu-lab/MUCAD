3dfd13b6c6b13f00ed475d3d5de56c2eb5ea93fe
jhliu-lab <jhliu.ai@gmail.com>
2026-03-10 17:54:47 +0800

    Update run_visa.sh

--- run_visa.sh
+++ run_visa.sh
@@ -4,7 +4,5 @@ conda activate pytorch1.12.1_ljh_mucad_1
 datapath=/home/ljh/datasets/visa
 datasets=('candle' 'capsules' 'cashew' 'chewinggum' 'fryum' 'macaroni1'
 'macaroni2' 'pcb1' 'pcb2' 'pcb3' 'pcb4' 'pipe_fryum')
-# datasets=('chewinggum')
 dataset_flags=($(for dataset in "${datasets[@]}"; do echo '-d '$dataset; done))
 CUDA_VISIBLE_DEVICES=1 python3 run_mucad.py --gpu 0 --seed 0 --memory_size 9800 --log_group visa_9800_50epoch_test  --save_segmentation_images --log_project visa  second_paper ucad -b wideresnet50 -le layer2 -le layer3 --faiss_on_gpu --pretrain_embed_dimension 1024 --target_embed_dimension 1024 --anomaly_scorer_num_nn 1 --patchsize 1 sampler -p 0.1 approx_greedy_coreset dataset --resize 224 --imagesize 224 --csv_path /home/ljh/datasets/visa/split_csv/1cls.csv  "${dataset_flags[@]}" visa $datapath 
-#memery_bank_alpha_0.5-vison_prompt_text_prompt_24_vison_layers-3-noise_level_1.0_fusion_model_50_epoch_adapt_sigmoid 
